544bcdeb4c8e8ad7d0ff5ea6d89434984efe4ef0
Jiajia Wang <grace.wangjiajia@gmail.com>
2021-10-07 21:09:47 -0700

    1st version

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ import random
 
 player = Player()
 
+
 def display(statement):
     print(statement)
 
@@ -14,12 +15,13 @@ def create_adadachi():
     foods = player.inventory["foods"]
     games = player.inventory["games"]
     personality = {
-        "fav_food": random.randint(0,len(foods)),
-        "fav_game": random.randint(0,len(games)),
-        "hates_food": random.randint(0,len(foods)),
-        "hates_game": random.randint(0,len(games)),
+        # "fav_food": random.randint(0,len(foods)),
+        "fav_food": "5",
+        "fav_game": random.randint(0, len(games)),
+        "hates_food": random.randint(0, len(foods)),
+        "hates_game": random.randint(0, len(games)),
     }
-    player.adadachi = Adadachi(name,personality)
+    player.adadachi = Adadachi(name, personality)
 
 
 def start_game():
@@ -31,17 +33,27 @@ def start_game():
             option = input(OPTIONS).lower()
 
             if option == "s":
-                player.get_status()
+                if player.get_status():
+                    display(WON)
+                else:
+                    print("Can we play one more time?")
+
             elif option == "c":
-                    player.clean()
+                player.clean()
+                if player.clean():
+                    print("I dont like shower")
             elif option == "f":
                 if player.inventory["foods"]:
-                    player.feed()
+                    if player.feed() == player.adadachi.personality["fav_food"]:
+                        display(WON)
                 else:
                     break
             elif option == "p":
                 player.play_with_adadachi()
+            elif player.break_hachi():
+                display(LOST)
+
             elif option == "x":
                 return display(EXIT)
-        
-        display(LOST)
+
+        # display(LOST)
--- player.py
+++ player.py
@@ -1,5 +1,7 @@
 from constants import STATUS
+from adadachi import Adadachi
 import random
+
 
 class Player:
     def __init__(self):
@@ -8,3 +10,40 @@ class Player:
             "games": ["hide-n-seek", "tag", "go fish", "red rover"],
             "foods": ["banana cream pie", "carrot sticks", "mashed potatoes", "mac 'n cheese", "tater tots", "chocolate cake", "strawberries", "fried rice"],
         }
+        # self.hungry = Adadachi(self.name, self.personality)
+
+    def play_with_adadachi(self):
+        count = 0
+        while count < 100:
+            game = input(
+                "which one you want to play? Options: 1.hide-n-seek, 2.tag, 3.go fish, 4.red rover, 5.stop")
+            if game == "5":
+                break
+            if game == "1":
+                print("Game started! let's hide-n-seek")
+            if game == "2":
+                print("Game started! let's tag")
+            if game == "3":
+                print("Game started! let's go fish")
+            if game == "4":
+                print("Game started! let's red rover")
+            count += 1
+        return count
+
+    def get_status(self):
+        play_times = self.play_with_adadachi()
+        if play_times > 3:
+            return True
+
+    def feed(self):
+        food = input("what do you want to eat? 1.banana cream pie , 2.carrot sticks, 3.mashed potatoes, 4.mac 'n cheese, 5.tater tots, 6.chocolate cake, 7.strawberries, 8.fried rice")
+        return food
+
+    def clean(self):
+        return True
+
+    def break_hachi(self):
+        if self.clean():
+            return True
+        else:
+            False
